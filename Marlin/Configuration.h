db842903d131ec52bc5f9af9fbc5a2b2c2ddfe0a
Staal6 <131151224+Staal6@users.noreply.github.com>
2023-08-10 10:49:09 +0200

    updateVersion

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -120,7 +120,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "MyConfiguration"
+#define CUSTOM_MACHINE_NAME "NuweVersion20230810"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
